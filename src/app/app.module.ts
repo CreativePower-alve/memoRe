b449b49f8115998488f02c64e42650cf0109875a
Alina <alina.mihai90@gmail.com>
2017-02-28 18:12:16 +0200

    first route component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule} from '@angular/material';
+import { MaterialModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 
 import 'hammerjs';
+import { ThingsComponent } from './things/things.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    SideNavComponent
+    SideNavComponent,
+    ThingsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+     RouterModule.forRoot([
+      { path: 'things', component: ThingsComponent },
+      { path: '', redirectTo: 'things', pathMatch: 'full' },
+      { path: '**', redirectTo: 'things', pathMatch: 'full' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
