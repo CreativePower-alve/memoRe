de0553dbb39032d390940d3b37075c2d1003bdd6
Alina <alina.mihai90@gmail.com>
2017-03-02 08:14:48 +0200

    refactor module structure

---
+++ src/app/things/things.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ThingsComponent } from './things.component';
+
+@NgModule({
+imports: [],
+declarations: [
+ThingsComponent
+]
+})
+export class ThingsModule {};
