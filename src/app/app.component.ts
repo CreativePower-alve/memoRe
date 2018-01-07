222ed7e408d198c59cd007c35be25250e9e90cd8
alinaMihai <alina_mihai90@yahoo.com>
2018-01-07 17:22:45 +0200

    hide sidenav on small screen sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { ThingsSessionService, SessionConfig } from './shared/things-session.service';
-import { LoginService } from './account/login/login.service';
-import { AuthTokenService } from './shared/authToken.service';
+import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+import {
+  ThingsSessionService,
+  SessionConfig
+} from "./shared/things-session.service";
+import { LoginService } from "./account/login/login.service";
+import { AuthTokenService } from "./shared/authToken.service";
 import { TagsService } from "./shared/tags.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   public isOpen: boolean;
@@ -18,17 +21,20 @@ export class AppComponent {
     private router: Router,
     private thingsSessionService: ThingsSessionService,
     public auth: LoginService,
-    private tagService: TagsService) {
-  }
+    private tagService: TagsService
+  ) {}
 
   ngOnInit() {
     if (this.authTokenService.isLoggedIn()) {
-      this.auth.checkAuthenticationStatus().subscribe((currentUser) => {
+      this.auth.checkAuthenticationStatus().subscribe(currentUser => {
         this.canShowNavigation();
       });
     }
-    this.router.events.subscribe((event) => { 
+    this.router.events.subscribe(event => {
       this.canShowNavigation();
+    });
+    this.addEvent(window, "resize", e => {
+      this.isOpen = window.innerWidth > 1000 && this.canShowNavBarButtons;
     });
   }
 
@@ -37,22 +43,20 @@ export class AppComponent {
   }
 
   ngAfterContentInit() {
-    this.authTokenService.dynamicSideNavEvent
-      .take(1)
-      .subscribe(() => {
-        setTimeout(() => {
-          this.isOpen = window.innerWidth > 600 && this.authTokenService.isLoggedIn();
-              
-        }, 0);
-        this.auth.checkAuthenticationStatus().subscribe();
-      });
+    this.authTokenService.dynamicSideNavEvent.take(1).subscribe(() => {
+      setTimeout(() => {
+        this.isOpen =
+          window.innerWidth > 600 && this.authTokenService.isLoggedIn();
+      }, 0);
+      this.auth.checkAuthenticationStatus().subscribe();
+    });
   }
 
   canShowNavigation() {
     if (this.auth.isAuthenticated()) {
       this.isOpen = window.innerWidth > 600;
     }
-    this.canShowNavBarButtons = location.pathname !== '/things-session';
+    this.canShowNavBarButtons = location.pathname !== "/things-session";
     this.isOpen = this.canShowNavBarButtons ? this.isOpen : false;
   }
 
@@ -64,13 +68,25 @@ export class AppComponent {
     this.thingsSessionService.setSessionConfig(confObject);
     if (confObject) {
       this.isOpen = false;
-      this.router.navigate(['/things-session']);
+      this.router.navigate(["/things-session"]);
     }
   }
   logout(): void {
     this.auth.logout();
     this.isOpen = false;
-    this.router.navigate(['/login']);
+    this.router.navigate(["/login"]);
   }
 
+  addEvent(object, eventType, callback) {
+    if (object == null || typeof object === "undefined") {
+      return;
+    }
+    if (object.addEventListener) {
+      object.addEventListener(eventType, callback, false);
+    } else if (object.attachEvent) {
+      object.attachEvent("on" + eventType, callback);
+    } else {
+      object["on" + eventType] = callback;
+    }
+  }
 }
